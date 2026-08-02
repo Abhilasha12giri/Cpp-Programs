3fb1b0bdd826f0070b99f5b599c174330669e4ed
Abhilasha Kumari <veeragoswami0563@gmail.com>
2026-08-02 19:32:44 +0530

    Added tut12pointer.cpp

---
+++ tut12pointer.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // What is pointer ? ----> data type which holds the address of other data types 
+
+   // int a =3;
+   // int* b = &a;
+   int a =3;
+    int* b ;
+    b = &a;
+      // & ----> (Address of) operator 
+    cout<<"The address of a is : "<<&a<<endl;
+    cout<<"The address of a is : "<<b<<endl;
+
+    // * ----> (value at) Derefrence operator 
+    cout<<"The value at address b is : "<<*b<<endl;
+
+    // Pointer to pointer ----> store address of another address
+    int** c = &b;
+    cout<<"The address of b is : "<<&b<<endl;
+    cout<<"The address of b is : "<<c<<endl;
+    cout<<"The value at address c is : "<<*c<<endl;
+    cout<<"The value at address value_at(value_at(c)) is : "<<**c<<endl; 
+    return 0;
+}
